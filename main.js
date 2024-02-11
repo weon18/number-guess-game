238feffed202199aba945cce7a701f6477c9e3ec
Dowon Jeong <aotlfrhd1004@naver.com>
2024-02-11 20:10:28 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+//랜덤번호 지정
+//유저가 번호를 입력한다 그리고 go 라는 버튼을 누름
+//만약에 유저가 랜덤번호를 맞추면, 맞췄습니다!
+//랜덤번호가 < 유저번호 Down!!
+//랜덤번호가 > 유저번호 Up!!
+//Reset버튼을 누르면 게임이 리셋된다
+//5번의 기회를 다 쓰면 게임이 끝난다 (더이상 추측 불가, 버튼이 disable)
+//유저가 1~100 범위 밖에 숫자를 입력하면 알려준다. 기회를 깎지 않는다
+//유저가 이미 입력한 숫자를 또 입력하면 알려준다. 기회를 깎지 않는다
+
+let computerNum = 0
+let buttonClick = document.getElementById("button-click");
+let userInput = document.getElementById("user-input");
+let resultArea = document.getElementById("result-area");
+
+buttonClick.addEventListener("click", click);
+
+function pickRandomNum(){
+    computerNum = Math.floor(Math.random()*100) + 1;
+    console.log("정답", computerNum);
+}
+
+function click(){
+    let userValue = userInput.value;
+    if(userValue < computerNum){
+        resultArea.textContent = "Up!!!"
+    }else if(userValue > computerNum){
+        resultArea.textContent = "Down!!!"
+    }else {
+        resultArea.textContent = "맞추셨습니다!!!"
+    }
+}
+
+pickRandomNum();
