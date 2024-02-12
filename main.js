534bcc6c9cf5fb1873d3817797a654f871b0239a
Dowon Jeong <aotlfrhd1004@naver.com>
2024-02-12 18:18:42 +0900

    second commit

--- main.js
+++ main.js
@@ -12,8 +12,15 @@ let computerNum = 0
 let buttonClick = document.getElementById("button-click");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 5;
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area");
+let history = [];
 
 buttonClick.addEventListener("click", click);
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus", function(){userInput.value=""})
 
 function pickRandomNum(){
     computerNum = Math.floor(Math.random()*100) + 1;
@@ -22,13 +29,45 @@ function pickRandomNum(){
 
 function click(){
     let userValue = userInput.value;
+
+    if(userValue < 1 || userValue > 100) {
+        resultArea.textContent="1과 100사이 숫자를 입력해주세요!"
+        return;
+    }
+    if(history.includes(userValue)){
+        resultArea.textContent="이미 입력한 숫자입니다. 다른 숫자를 입력해주세요!"
+        return;
+    }
+
+    chances -- ;
+    chanceArea.textContent = "남은기회 : "+chances+"번"; //`남은기회:${chances}번`; *chances:동적인값 과 정적인 값을 합쳐서 쓸 수 있는 문법
+
     if(userValue < computerNum){
         resultArea.textContent = "Up!!!"
     }else if(userValue > computerNum){
         resultArea.textContent = "Down!!!"
     }else {
         resultArea.textContent = "맞추셨습니다!!!"
+        gameOver=true
+    }
+
+    history.push(userValue);
+
+    if(chances < 1){
+        gameOver = true;
+    }
+
+    if(gameOver == true){
+        buttonClick.disabled = true;
     }
 }
 
+function reset(){
+    //user input창이 깨끗하게 정리
+    userInput.value = ""
+    //새로운 번호가 생성
+    pickRandomNum();
+
+    resultArea.textContent="결과값이 여기 나옵니다."
+}
 pickRandomNum();
